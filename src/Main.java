aa18e918d39480402c5e2833cba5d46c2d02d0a3
AleAlexander <pa998@yandex.ru>
2019-07-06 16:20:24 +0300

    Добавлено появление окна по центру экрана

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Main {
@@ -7,6 +8,9 @@ public class Main {
         applet.init();
         JFrame frame  = new JFrame();
         frame.getContentPane().add(applet);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        frame.setLocation(dimension.width/2 - 400, dimension.height/2 - 250);
         frame.setTitle("JGraphT Adapter to JGraphX Demo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
