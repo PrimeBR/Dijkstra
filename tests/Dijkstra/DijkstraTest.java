94b27f9373b62030a452e979f3853356c0207e00
AleAlexander <pa998@yandex.ru>
2019-07-10 05:11:13 +0300

    Небольшие дополнения

---
+++ tests/Dijkstra/DijkstraTest.java
@@ -0,0 +1,63 @@
+package Dijkstra;
+
+import org.junit.*;
+import com.mxgraph.model.mxCell;
+import com.mxgraph.view.mxGraph;
+
+import static org.junit.Assert.*;
+
+public class DijkstraTest {
+
+
+    @BeforeClass
+    public static void beforeClass() {
+
+        System.out.println("Before testing");
+
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After testing");
+    }
+
+    @Test
+    public void testAlgo(){
+
+    }
+
+/*
+    @Test
+    public void getStep() {
+    }
+
+    @Test
+    public void isNextStep() {
+    }
+
+    @Test
+    public void removeVertex() {
+    }
+
+    @Test
+    public void selectUnvisitedVertex() {
+    }
+
+    @Test
+    public void selectNearestNeighbor() {
+    }
+
+    @Test
+    public void relax() {
+    }
+
+    @Test
+    public void pathRestoration() {
+    }
+
+    @Test
+    public void toString1() {
+    }
+
+   */
+}
